aa2def6e45913899d145472885e760c52070a567
Anwesh43 <anweshthecool0@gmail.com>
2017-03-28 03:54:03 +0530

    adding AnimQueue for triggering animation step by step

--- src/AnimQueue.js
+++ src/AnimQueue.js
@@ -2,22 +2,62 @@ import React,{Component} from 'react'
 class Transformation extends Component {
     constructor(props) {
         super(props)
+        this.state = {style:[]}
     }
 }
 export default class AnimQueue extends Component {
     constructor(props) {
         super(props)
-        this.state = {queue:[]}
+        this.state = {queue:[],style:this.props.style}
     }
     componentDidMount() {
-        console.log(this.props.children)
         const childrenProps = this.props.children.map((child)=>child.props)
-        const childrenRef = this.props.children.map((child)=>child.refs)
-        console.log(childrenProps)
-        console.log(childrenRef)
+
+        var queue = this.state.queue
+        childrenProps.forEach((childProps)=>{
+            if(childProps.type!=undefined  && childProps.to!=undefined && childProps.from!=undefined && childProps.axis!=undefined && childProps.duration!=undefined) {
+                var queueObj = {}
+                queueObj.lastPart = 'px'
+                if(childProps.type=='rotate') {
+                    queueObj.lastPart = 'deg'
+                }
+                queueObj.transformFn = `${childProps.type}${childProps.axis}`
+                queueObj.counter = 0
+                queueObj.till = childProps.duration/100
+                queueObj.speed = (childProps.to - childProps.from)/queueObj.till
+                queueObj.curr = parseInt(childProps.from)
+                queue.push(queueObj)
+            }
+        })
+        this.setState({queue})
+        var prevTransform = ""
+        const intervalObj = setInterval(()=>{
+            var queue = this.state.queue
+            var style = Object.assign({},this.state.style)
+            console.log(queue)
+            console.log(style)
+            if(queue.length>0 && style!=undefined) {
+                var firstObj = queue[0]
+                style.WebkitTransform = `${prevTransform}${firstObj.transformFn}(${firstObj.curr}${firstObj.lastPart})`
+                style.MozTransform = `${prevTransform}${firstObj.transformFn}(${firstObj.curr}${firstObj.lastPart})`
+                style.OTransform = `${prevTransform}${firstObj.transformFn}(${firstObj.curr}${firstObj.lastPart})`
+                style.transform = `${prevTransform}${firstObj.transformFn}(${firstObj.curr}${firstObj.lastPart})`
+                firstObj.curr += parseInt(firstObj.speed)
+                firstObj.counter++
+                if(firstObj.counter == firstObj.till+1) {
+                    queue.splice(0,1)
+                    prevTransform = style.transform
+                }
+                this.setState({style,queue})
+            }
+            else {
+                clearInterval(intervalObj)
+            }
+
+        },100)
     }
     render() {
-        return <div style={this.props.style}></div>
+        return <div style={this.state.style}></div>
     }
 }
 export {Transformation}
